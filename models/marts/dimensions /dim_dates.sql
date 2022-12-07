33afac00e8dd695b33706be8e93682d3d42b3923
Daniil Mikheev <dan983m@gmail.com>
2022-12-07 23:52:21 +0000

    eviction_dim

---
+++ models/marts/dimensions /dim_dates.sql
@@ -0,0 +1,20 @@
+{{ config (
+    materialized="table"
+)}}
+
+with dates_dim as (
+{{ dbt_utils.date_spine(
+    datepart="day",
+    start_date="cast('2017-01-03' as date)",
+    end_date="cast('2020-04-29' as date)"
+   )
+}})
+
+select row_number() OVER () AS date_dim_id,
+    date_day as date_value,
+    EXTRACT(MONTH FROM date_day) AS month,
+    EXTRACT(DAY FROM date_day) AS day,
+    EXTRACT(YEAR FROM date_day) AS year
+from dates_dim
+order by 1
+
